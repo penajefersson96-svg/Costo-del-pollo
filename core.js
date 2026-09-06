7a5d6406cf01d4500a97cd927c58d806586d0237
penajefersson96-svg <penajefersson96@gmail.com>
2026-09-06 12:02:03 -0400

    update core.js

--- core.js
+++ core.js
@@ -67,7 +67,7 @@ function canvasReporte(nombre,r){
   y+=150;
   x.fillStyle=r.gan>=0?'#1E7A3C':'#C0392B';x.font='800 64px '+F;
   x.fillText((r.gan>=0?'Ganancia ':'Pérdida ')+smart(Math.abs(r.gan)),60,y);
-  x.fillStyle='#61706A';x.font='600 26px '+F;x.fillText('Generado con Precio del Pollo',60,H-50);
+x.fillText('Generado con '+negocio()+' · Sistema Avícola',60,H-50);
   return c;
 }
 async function compartirReporte(nombre,r){
